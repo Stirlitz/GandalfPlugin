95367b2697fb75662d9ff95e1e8e91a1a8e289e2
Stirlitz <fajner1@gmail.com>
2014-04-26 19:48:31 -0600

    So far, "Gandalf" and the "Balrog" spawn, the plugin pauses two seconds, and Gandalf delivers his initial line.
    Both mobs are killed onDisable().

--- gandalf/src/main/java/gandalf/gandalf.java
+++ gandalf/src/main/java/gandalf/gandalf.java
@@ -1,32 +1,18 @@
 package gandalf;
 
-import java.util.HashMap;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.NPC;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Villager;
-import org.bukkit.entity.Zombie;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-
-import de.ntcomputer.minecraft.controllablemobs.api.ControllableMob;
-import de.ntcomputer.minecraft.controllablemobs.api.ControllableMobs;
+import org.bukkit.scheduler.BukkitScheduler;
 
 public class gandalf extends JavaPlugin {
 	
@@ -40,6 +26,8 @@ public class gandalf extends JavaPlugin {
 		}
 	}*/
 	
+	LivingEntity bro, balrog;
+	
 	
 	 @Override
 	 public void onEnable() {
@@ -48,6 +36,11 @@ public class gandalf extends JavaPlugin {
 	 @Override
 	 public void onDisable() {
 	     getLogger().info("gandalf.onDisable has been invoked!");
+	     try {
+	    	 bro.damage(10000);
+	    	 balrog.damage(10000);
+	     } catch (NullPointerException e) {}
+	     
 	 }
 
 	 @Override
@@ -58,11 +51,23 @@ public class gandalf extends JavaPlugin {
 	             Location loc = player.getLocation();
 	             loc.setX(loc.getX() + 5);
 	             World w = Bukkit.getWorld("world");
-	             LivingEntity bro = (LivingEntity) w.spawnEntity(loc, EntityType.VILLAGER);
-	             bro.addPotionEffect(new PotionEffect(PotionEffectType.SLOWNESS, 2000, 5));
+	             bro = (LivingEntity) w.spawnEntity(loc, EntityType.VILLAGER);
+	             bro.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 2000, 5));
+	             loc.setZ(loc.getZ() + 10);
+	             balrog = (LivingEntity) w.spawnEntity(loc, EntityType.GIANT);
+	             balrog.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 2000, 5));
 //	             Block b = loc.getBlock();
 //	             b.setType(Material.STONE);
-	             Bukkit.getServer().broadcastMessage(ChatColor.LIGHT_PURPLE + /*player.getName() + */": You cannot pass!" );
+	             
+	             BukkitScheduler scheduleA = Bukkit.getServer().getScheduler();
+	             scheduleA.scheduleSyncDelayedTask(this, new Runnable() {
+	            	 public void run() {
+	    	             Bukkit.getServer().broadcastMessage(ChatColor.LIGHT_PURPLE + /*player.getName() + */"You cannot pass!" );
+					} 
+	             }, 40L);
+	             
+	             
+
 	             return true;
 	         }
 	         else {
