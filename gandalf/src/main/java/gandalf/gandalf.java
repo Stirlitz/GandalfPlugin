6bcd4d227ba14ab8233192e9b573ec24779c00df
Stirlitz <fajner1@gmail.com>
2014-04-26 18:17:14 -0600

    Started using Controllable Mobs API

--- gandalf/src/main/java/gandalf/gandalf.java
+++ gandalf/src/main/java/gandalf/gandalf.java
@@ -1,5 +1,7 @@
 package gandalf;
 
+import java.util.HashMap;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -8,16 +10,30 @@ import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
+import org.bukkit.entity.Zombie;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import de.ntcomputer.minecraft.controllablemobs.api.ControllableMob;
+import de.ntcomputer.minecraft.controllablemobs.api.ControllableMobs;
+
 public class gandalf extends JavaPlugin {
+	
+	 private HashMap<Player,ControllableMob<Villager>> villagerMap;
+	 
+	 private void spawnVillager(Player owner, Location spawnLocation) {
+		 Villager villager = spawnLocation.getWorld().spawn(spawnLocation, Villager.class);
+		 ControllableMob<Villager> controlledVillager = ControllableMobs.assign(villager, true);
+	 }
+	 
 	 @Override
 	 public void onEnable() {
 	     getLogger().info("gandalf.onEnable has been invoked!");
+	     this.villagerMap = new HashMap<Player,ControllableMob<Villager>>();
 	 }
 	 @Override
 	 public void onDisable() {
@@ -31,12 +47,12 @@ public class gandalf extends JavaPlugin {
 	             Player player = (Player)sender;
 	             Location loc = player.getLocation();
 	             loc.setX(loc.getX() + 5);
-	             World world = null;
-	             LivingEntity bro = world.spawnCreature(loc, EntityType.VILLAGER);
+	             World w = Bukkit.getWorld("world");
+	             this.spawnVillager(loc);
+//	             Entity bro = w.spawnEntity(loc, EntityType.VILLAGER);
 //	             Block b = loc.getBlock();
 //	             b.setType(Material.STONE);
-	             Bukkit.getServer().broadcastMessage(ChatColor.LIGHT_PURPLE + player.getName() + ": You cannot pass!" );
-	             // DO SOMETHING HERE
+	             Bukkit.getServer().broadcastMessage(ChatColor.LIGHT_PURPLE + /*player.getName() + */": You cannot pass!" );
 	             return true;
 	         }
 	         else {
@@ -46,6 +62,8 @@ public class gandalf extends JavaPlugin {
 	     }
 	     return false;
 	 }
+	 
+
 
 
 
